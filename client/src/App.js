ba5fc226a132ed666d1c44354b51fb18b0bbb33b
Romserg <serg.romankov@gmail.com>
2020-02-10 01:25:50 +0200

    Prepare mocks for project

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 import Jobs from "./Jobs";
 
+const mockJobs = [
+  {title: 'SWE 1', company: 'Google'}
+];
+
 function App() {
   return (
     <div className="App">
-      <Jobs/>
+      <Jobs jobs={mockJobs}/>
     </div>
   );
 }
