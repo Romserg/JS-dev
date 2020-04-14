efca25a2852151ba984498776897e753fbce590a
Romserg <serg.romankov@gmail.com>
2020-04-15 02:17:27 +0300

    Prepare pagination block without logic

--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,8 +1,24 @@
 import React from "react";
 import Typography from '@material-ui/core/Typography';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import Job from "./Job";
 
 export default function Jobs({jobs}) {
+  const numJobs = jobs.length;
+  const numPages = Math.ceil(numJobs / 50);
+
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
   console.log('job is', jobs[0]);
 
@@ -16,6 +32,27 @@ export default function Jobs({jobs}) {
           job => <Job job={job} />
         )
       }
+      <div>
+        Page {activeStep + 1} of {numPages}
+      </div>
+      <MobileStepper
+        variant="progress"
+        steps={numPages}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+            Next
+            <KeyboardArrowRight />
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            <KeyboardArrowLeft />
+            Back
+          </Button>
+        }
+      />
     </div>
   )
 }
