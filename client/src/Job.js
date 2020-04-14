41dee3f017a4afda65a1c956779259a52cf72784
Romserg <serg.romankov@gmail.com>
2020-04-15 01:48:15 +0300

    Improve markup and some styles

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Paper, Typography } from "@material-ui/core";
 
 export default function Job({job}) {
-  return(
-    <div className={'job'}>
-      <span>{job.title}</span>
-      <span>{job.company}</span>
-    </div>
+  return (
+    <Paper className='job'>
+      <div>
+        <Typography variant='h5'>{job.title}</Typography>
+        <Typography variant='h6'>{job.company}</Typography>
+        <Typography>{job.location}</Typography>
+      </div>
+      <div>
+        <Typography>{job.created_at.split(' ').splice(0, 3).join(' ')}</Typography>
+      </div>
+    </Paper>
   )
 }
